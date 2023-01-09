a012fe78d691570fa65d5b82715c8b96e16ce8d1
Arun Nair <davor.suker09@gmail.com>
2023-01-08 20:16:40 -0600

    destructuring

--- nucampsite/src/features/campsites/CampsiteCard.js
+++ nucampsite/src/features/campsites/CampsiteCard.js
@@ -1,14 +1,15 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-const CampsideCard = (props) => {
+const CampsideCard = ({ campsite }) => {
+    const [image, name] = campsite;
     return ( 
         <Card>
             <CardImg
                 width='100%'
-                src={props.campsite.image}
-                alt={props.campsite.name} />
+                src={image}
+                alt={name} />
             <CardImgOverlay>
-                <CardTitle>{props.campsite.name}</CardTitle>
+                <CardTitle>{name}</CardTitle>
             </CardImgOverlay> 
         </Card>
      );
