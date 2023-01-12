6ef1f9636909d250d800a8161cc471b1ef14fd77
Arun Nair <davor.suker09@gmail.com>
2023-01-12 09:10:31 -0600

    hooks useState

--- nucampsite/src/features/campsites/CampsitesList.js
+++ nucampsite/src/features/campsites/CampsitesList.js
@@ -2,13 +2,17 @@ import { selectAllCampsites } from "./campsitesSlice";
 import CampsideCard from "./CampsiteCard";
  import { Row, Col } from 'reactstrap';
 
-const CampsitesList = () => {
+const CampsitesList = ({setCampsiteId}) => {
     const campsites = selectAllCampsites();
     return (
         <Row className='ms-auto'>
             {campsites.map((campsite) => {
                 return (
-                    <Col md='5' className='m-4' key={campsite.id}>
+                    <Col
+                        md='5'
+                        className='m-4'
+                        key={campsite.id}
+                        onClick={() => setCampsiteId(campsite.id)}>
                         <CampsideCard campsite={campsite}></CampsideCard>
                     </Col>
                 );
