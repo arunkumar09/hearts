f9e3cd18d442dbe0639583d2c80afdb4ebc778e1
Arun Nair <davor.suker09@gmail.com>
2023-01-12 09:36:35 -0600

    react router

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <CampsitesDirectoryPage />
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='contact' element={<ContactPage />}/>
+        <Route path='directory' element= {<CampsitesDirectoryPage/>}/>
+      </Routes>
       <Footer />
     </div>
   );
