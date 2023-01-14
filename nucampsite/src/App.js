29e970656f7148240c348bbc96ce30dac6c9ff34
Arun Nair <davor.suker09@gmail.com>
2023-01-14 10:42:19 -0600

    task 1, task 2 workshop 2

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -6,6 +6,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
         <Route path='/' element={<HomePage/>} />
         <Route path='contact' element={<ContactPage />}/>
         <Route path='directory' element={<CampsitesDirectoryPage />} />
-        <Route path='directory/:campsiteId' element= {<CampsiteDetailPage/>}/>
+        <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+        <Route path='about' element={<AboutPage />} />
       </Routes>
       <Footer />
     </div>
