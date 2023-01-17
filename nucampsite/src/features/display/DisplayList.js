9e00fd51b1baaa44ce4cef06af58f842e6485927
Arun Nair <davor.suker09@gmail.com>
2023-01-17 14:49:16 -0600

    animated display

--- nucampsite/src/features/display/DisplayList.js
+++ nucampsite/src/features/display/DisplayList.js
@@ -2,17 +2,19 @@ import { Col, Row } from 'reactstrap';
 import { selectFeaturedCampsite } from '../campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import { selectFeaturedPartner } from '../partners/partnersSlice';
-import DisplayCard from './DisplayCard';
+// import DisplayCard from './DisplayCard';
+import AnimatedDisplayCard from './AnimatedDisplayCard';
 
 const DisplayList = () => {
     const items = [selectFeaturedCampsite(), selectFeaturedPromotion(), selectFeaturedPartner()];
     return (
         <Row>
             {items.map((item, idx) => {
-                return (
+                return ( item && (
                     <Col md className='m-1' key={idx}>
-                        <DisplayCard item={item}/>
+                        <AnimatedDisplayCard item={item} />
                     </Col>
+                    )
                 );
             })}
         </Row> );
